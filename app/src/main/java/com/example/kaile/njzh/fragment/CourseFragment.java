c6a073d23c9fdb055e54a53b315511b6b27455df
kaile <kaile_sun@126.com>
2017-10-09 10:49:20 +0800

    main

---
+++ app/src/main/java/com/example/kaile/njzh/fragment/CourseFragment.java
@@ -0,0 +1,22 @@
+package com.example.kaile.njzh.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.kaile.njzh.R;
+
+/**
+ * Created by kaile on 2017/10/8.
+ */
+
+public class CourseFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.course_fragment, container, false);
+    }
+}
